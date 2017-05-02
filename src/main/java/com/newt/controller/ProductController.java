3a1148120bf3379978b20636bb4ebc11f8d0be20
newtglobaluser <newtglobaluser@gmail.com>
2017-05-02 16:05:54 +0530

    Update ProductController.java

--- src/main/java/com/newt/controller/ProductController.java
+++ src/main/java/com/newt/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+
 import com.newt.model.Product;
 import com.newt.service.ProductService;
 import com.wordnik.swagger.annotations.ApiOperation;
